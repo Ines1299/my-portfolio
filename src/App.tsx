543dac4e3ee8b60eb763b6f364e9fc6602615bce
Ines Cadete <inesmcadete@hotmail.com>
2026-05-05 13:00:59 +0100

    installed core libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div>
-      <h1>My Portfolio</h1>
+    <div className="bg-black min-h-screen flex items-center justify-center">
+      <h1 className="text-white text-6xl font-bold">My Portfolio</h1>
     </div>
   );
 }
