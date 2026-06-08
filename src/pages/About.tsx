ea66e54e680ea9c9829e615cf9813e7c2d18affc
Ines Cadete <inesmcadete@hotmail.com>
2026-06-09 00:23:06 +0100

    Started About page structure

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,21 @@
 function About() {
   return (
-    <div className="container">
-      <h1>About</h1>
+    <div className="flex mb-4">
+      <div className="w-1/2">
+        <h1>About me</h1>
+        <p>
+          First Class Illustration and Visual Media graduate from UAL, now
+          looking to work as a Junior Developer after completing an intensive
+          JavaScript bootcamp at Northcoders. I bring a rare combination of
+          design thinking and full-stack technical skills, comfortable moving
+          between Figma and React, and between creative briefs and codebases.
+          Passionate about building accessible, intuitive interfaces where clean
+          code and thoughtful UX go hand in hand.
+        </p>
+      </div>
+      <div className=" w-1/2 h-96 border border-black flex items-center justify-center shrink-0 ">
+        <p className="text-xs">[my photo goes here]</p>
+      </div>
     </div>
   );
 }
